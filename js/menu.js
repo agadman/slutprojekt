0b332f4b37c370b46fcff877ae2a75e3c0c532d1
Annika Gadman <annika.gadman@gmail.com>
2022-01-25 21:10:10 -0600

    made some corrections

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,6 @@
+//Hamburgermeny vid mindre skärmar
 function myFunction() {
-    var x = document.getElementById("navbar");
+    let x = document.getElementById("navbar");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
--- js/notification.js
+++ js/notification.js
@@ -1,3 +1,4 @@
+// Använder JQUERY för att fada in notifieringen på index-sidan
 $(document).ready(function() {
     $("div.notification").fadeIn(2000);
 })
