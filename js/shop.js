74452546cc9590251c5f3f4e2dd4497cc5f2e4aa
Annika Gadman <annika.gadman@gmail.com>
2022-01-27 17:01:11 -0600

    changed the modal JS script

--- js/shop.js
+++ js/shop.js
@@ -1,15 +1,13 @@
 // Get the modal
 var modal = document.getElementById('myModal');
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
+// Get the image and insert it inside the modal
 var img = document.getElementById('shopImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+var modalImg = document.getElementById("modal_img");
+
 img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
-    modalImg.alt = this.alt;
-    //captionText.innerHTML = this.alt;
 }
 
 // Get the <span> element that closes the modal
