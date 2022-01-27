bf8b517ec035f347a591f66b1447b1a16efd5a95
Annika Gadman <annika.gadman@gmail.com>
2022-01-27 16:49:19 -0600

    corrections

--- js/tasting_slideshow.js
+++ js/tasting_slideshow.js
@@ -7,14 +7,16 @@ function showSlides() {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
+
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2500); // Change image every 2.5 seconds
+  setTimeout(showSlides, 2500); // Byter bild varje 2.5 sekund
 }
 
 showSlides();
